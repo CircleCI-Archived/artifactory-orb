36a11248faca909da92ab6597082389b88093e44
Eddie Webb <ollitech@gmail.com>
2018-09-26 16:16:50 -0400

    publish to dev, test external import

--- tests/runner.sh
+++ tests/runner.sh
@@ -6,32 +6,42 @@
 
 set -e
 
+USAGE='\n
+USAGE:\n
+runner.sh config create-inline orbname filename - merge all orbs in src/ to file\n
+runner.sh config import-dev namespace orbname filename - declare dev orb from CIRCLE_BRANCH\n
+runner.sh test_against filename - use filename as "orbs" section against all test workflows\n
 
 
+'
 
-function main {
-	assemble_inline "artifactory" "inline.yml"
-	run_tests "inline.yml"
-}
+
 
 function assemble_inline {
 	ORB=$1
 	OUTPUT=$2
 	echo "version: 2.1" > $OUTPUT
 	echo "orbs:" >> $OUTPUT
-	echo "  artifactory:" >> $OUTPUT
+	echo "  $ORB :" >> $OUTPUT
 	circleci config pack src | sed -e 's/^/    /'>> $OUTPUT
 }
 
 
-function remove_files_in {
-	rm $1/temp-*
+
+function assemble_external {
+	NAMESPACE=$1
+	ORB=$2
+	OUTPUT=$3
+	LABEL=${4-$CIRCLE_BRANCH}
+	echo "version: 2.1" > $OUTPUT
+	echo "orbs:" >> $OUTPUT
+	echo "  $ORB: $NAMESPACE/$ORB@dev:${LABEL}" >> $OUTPUT 
 }
 
 
 function run_tests {
 	INLINE=$1
-	TMP_DIR=`mktemp -d -t orbs.XXXX`
+	MY_TMP_DIR=`mktemp -d -t orbs.XXXX`
 
 	fails=0
 	tests=0
@@ -40,20 +50,20 @@ function run_tests {
 		set -e
 		tests=$((tests+1))
 		echo "Running Test: ${file%.yml}"
-		cat ${INLINE} > ${TMP_DIR}/temp-input.yml
-		cat $file | sed -ne '/#given/,/#end given/p' | sed '1d;$d'>> ${TMP_DIR}/temp-input.yml
-		cat $file | sed -ne '/#then/,/#end then/p' | sed '1d;$d'> ${TMP_DIR}/expected.yml
-		circleci config process ${TMP_DIR}/temp-input.yml  2>${TMP_DIR}/temp-error.txt | sed '/# Original config.yml file:/q'| sed '$d' > ${TMP_DIR}/temp-output.yml 
+		cat ${INLINE} > ${MY_TMP_DIR}/temp-input.yml
+		cat $file | sed -ne '/#given/,/#end given/p' | sed '1d;$d'>> ${MY_TMP_DIR}/temp-input.yml
+		cat $file | sed -ne '/#then/,/#end then/p' | sed '1d;$d'> ${MY_TMP_DIR}/expected.yml
+		circleci config process ${MY_TMP_DIR}/temp-input.yml  2>${MY_TMP_DIR}/temp-error.txt | sed '/# Original config.yml file:/q'| sed '$d' > ${MY_TMP_DIR}/temp-output.yml 
 		set +e
-		if [ -s ${TMP_DIR}/temp-error.txt ];then
+		if [ -s ${MY_TMP_DIR}/temp-error.txt ];then
 			# processing generated error, check for match
-			ACTUAL=${TMP_DIR}/temp-error.txt
+			ACTUAL=${MY_TMP_DIR}/temp-error.txt
 		else
 			# no error, check generated config
-			ACTUAL=${TMP_DIR}/temp-output.yml
+			ACTUAL=${MY_TMP_DIR}/temp-output.yml
 		fi
 
-		diff -B $ACTUAL ${TMP_DIR}/expected.yml 
+		diff -B $ACTUAL ${MY_TMP_DIR}/expected.yml 
 		if [ $? -eq 0 ];then
 			echo -e "\tPass"
 		else
@@ -61,7 +71,7 @@ function run_tests {
 			fails=$((fails+1))
 		fi
 
-		remove_files_in ${TMP_DIR}
+		rm -R ${MY_TMP_DIR}/*
 	done
 	echo "Ran $tests tests with $fails failures"
 	if [ $fails -gt 0 ];then
@@ -69,5 +79,23 @@ function run_tests {
 	fi
 }
 
-
-main
+case "$1" in
+	config)
+		shift
+		case "$1" in
+			"create-inline")
+				assemble_inline $2 $3
+			;;
+			"import-dev")				
+				assemble_external $2 $3 $4
+			;;
+		esac
+	;;
+	test_against)
+		run_tests $2
+	;;
+	*)
+		echo -e $USAGE
+		exit 2
+	;;
+esac
